ea7b8ae1b1a25afc95fac71fa100f0bdc1a1c8d7
Akashooti <107711784+Akashooti@users.noreply.github.com>
2022-07-05 14:23:07 +0530

    Create MathHypot.java

---
+++ MathHypot.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class MathHypot
+{
+    public static void main(String[] args)
+    {
+double x = 6;
+double y=  8;
+double result = Math.hypot(x,y);
+System.out.println("Hypotenuse value is = " + result);
+}
+}
