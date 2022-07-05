5bd487f114ae7dc0f1fed9ddbabc6743c0a68b16
Akashooti <107711784+Akashooti@users.noreply.github.com>
2022-07-05 14:30:27 +0530

    Create Mathlog.java

---
+++ Mathlog.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class MathLog
+{
+    public static void main(String[] args)
+    {
+double x = 24;
+double log = Math.log(x);
+System.out.println("Log value is = " + log);
+double log10 = Math.log10(x);
+System.out.println("Log10 value is = " + log10);
+double log1p = Math.log1p(x);
+System.out.println("Log1p value is = " + log1p);
+}
+}
